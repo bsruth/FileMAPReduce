6a4ff9d0ed686d5f26cf77e9cb041f490fd79562
Brian <Brian@SMILODON>
2016-06-24 16:41:46 -0700

    using a generator to get data from the dat file

--- main.cpp
+++ main.cpp
@@ -15,10 +15,59 @@ struct Transition {
 	int frameNumber;
 };
 
+const uint64_t InvalidTS = 0xABABAB;
 struct Sample {
 	uint64_t timestamp;
 	int16_t value;
 };
+
+
+std::vector<Sample> GetSamplesFromDAT(const fs::path& filePath, int channelIndex, int numChannels, double timePerSample)
+{
+	if (fs::exists(filePath) == false) {
+		return std::vector<Sample>();
+	}
+
+	auto peakPred = [](const auto& prev, const auto& next) {
+		return (prev.value > 0 && prev.value > next.value)
+			|| (prev.value < 0 && prev.value < next.value);
+	};
+
+	auto findInflect = [](const auto& prev, const auto& next) {
+
+		auto lowToHigh = (prev.value <= 0) && (next.value > 0);
+		auto highToLow = (prev.value >= 0) && (next.value < 0);
+		return  lowToHigh || highToLow;
+	};
+
+	std::vector<Sample> samples;
+	FILE* layFile = nullptr;
+	fopen_s(&layFile, filePath.string().c_str(), "rb");
+	using SampleValueType = decltype(Sample::value);
+
+	int recordChunkSize = 10000;
+	size_t samplesPerChunk = numChannels * recordChunkSize;
+	std::vector<SampleValueType> dataBuffer(samplesPerChunk + recordChunkSize, 0); //one extra for padding
+	int sampleIndex = 0;
+	uint64_t sampleTimestamp = 0;
+	auto samplesRead = fread_s(dataBuffer.data(), dataBuffer.size() * sizeof(SampleValueType), sizeof(SampleValueType), samplesPerChunk, layFile);
+	if (samplesRead <= 0) {
+		return samples;
+	}
+	while (samplesRead > 0) {
+		for (size_t sampleOffset = channelIndex; sampleOffset < samplesRead; sampleOffset += numChannels) {
+			Sample sample;
+			sample.timestamp = sampleTimestamp;
+			sample.value = dataBuffer[sampleOffset];
+			samples.emplace_back(sample);
+			++sampleIndex;
+			sampleTimestamp = static_cast<uint64_t>(timePerSample * sampleIndex);
+		}
+		samplesRead = fread_s(dataBuffer.data(), dataBuffer.size() * sizeof(SampleValueType), sizeof(SampleValueType), samplesPerChunk, layFile);
+	}
+	fclose(layFile);
+	return samples;
+}
 
 
 std::vector<Transition> GetTransitionsFromDAT(const fs::path& filePath, int channelIndex, int numChannels, double timePerSample)
@@ -103,6 +152,87 @@ Sample MapValueToSample(int16_t* buffer, int channelToMap, uint64_t timestamp)
 	s.value = buffer[channelToMap];
 	return s;
 }
+
+class SampleGenerator {
+public:
+	SampleGenerator(FILE* file, int channelIndex, int numChannels, double timePerSample)
+		: file_(file)
+		, channelIndex_(channelIndex)
+		, numChannels_(numChannels)
+		, timePerSample_(timePerSample)
+	{
+		recordChunkSize = 10000;
+		samplesPerChunk = numChannels * recordChunkSize;
+		dataBuffer = std::vector<SampleValueType>(samplesPerChunk + recordChunkSize, 0); //one extra for padding
+		sampleIndex = 0;
+		sampleTimestamp = -timePerSample;
+		lastReadSampleIndex = 0;
+		maxCurrentSampleOffset = 0;
+
+	}
+	~SampleGenerator() {}
+
+	Sample GetNextSample()
+	{
+		++sampleIndex;
+		sampleTimestamp += timePerSample_;
+
+		if ( sampleIndex >= lastReadSampleIndex) {
+			auto samplesRead = fread_s(dataBuffer.data(), dataBuffer.size() * sizeof(SampleValueType), sizeof(SampleValueType), samplesPerChunk, file_);
+			if (samplesRead <= 0) {
+				Sample s;
+				s.timestamp = InvalidTS;
+				return s;
+			}
+			lastReadSampleIndex = (samplesRead / numChannels_);
+			sampleIndex = 0;
+		}
+		
+		Sample s;
+		auto offset = sampleIndex * numChannels_;
+		s.value = dataBuffer[offset];
+		s.timestamp = static_cast<uint64_t>(sampleTimestamp);
+		return s;
+	}
+
+private:
+	FILE* file_;
+	int channelIndex_;
+	int numChannels_;
+	double timePerSample_;
+
+	using SampleValueType = decltype(Sample::value);
+	int recordChunkSize = 10000;
+	size_t samplesPerChunk;
+	std::vector<SampleValueType> dataBuffer;
+	int sampleIndex;
+	double sampleTimestamp;
+	int lastReadSampleIndex;
+	int maxCurrentSampleOffset;
+	int samplesProcessed;
+};
+
+std::vector<Sample> GetSamplesFromDAT2(const fs::path& filePath, int channelIndex, int numChannels, double timePerSample) {
+	if (fs::exists(filePath) == false) {
+		return std::vector<Sample>();
+	}
+
+	std::vector<Sample> samples;
+
+	FILE* layFile = nullptr;
+	fopen_s(&layFile, filePath.string().c_str(), "rb");
+
+	SampleGenerator sGen(layFile, channelIndex, numChannels, timePerSample);
+
+	auto s = sGen.GetNextSample();
+	while (s.timestamp != InvalidTS) {
+		samples.push_back(s);
+		s = sGen.GetNextSample();
+	}
+	return samples;
+	fclose(layFile);
+
+}
 std::vector<Transition> GetTransitionsFromDAT2(const fs::path& filePath, int channelIndex, int numChannels, double timePerSample)
 {
 	if (fs::exists(filePath) == false) {
@@ -125,31 +255,19 @@ std::vector<Transition> GetTransitionsFromDAT2(const fs::path& filePath, int cha
 	std::vector<Sample> samples;
 	FILE* layFile = nullptr;
 	fopen_s(&layFile, filePath.string().c_str(), "rb");
-	using SampleValueType = decltype(Sample::value);
+	
+	SampleGenerator sGen(layFile, channelIndex, numChannels, timePerSample);
 
-	int recordChunkSize = 10000;
-	size_t samplesPerChunk = numChannels * recordChunkSize;
-	std::vector<SampleValueType> dataBuffer(samplesPerChunk + recordChunkSize, 0); //one extra for padding
-	int sampleIndex = 0;
-	uint64_t sampleTimestamp = 0;
-	auto samplesRead = fread_s(dataBuffer.data(), dataBuffer.size() * sizeof(SampleValueType), sizeof(SampleValueType), samplesPerChunk, layFile);
-	if (samplesRead <= 0) {
-		return transitions;
-	}
-
-	Sample prev;
-	prev.timestamp = sampleTimestamp;
-	prev.value = dataBuffer[channelIndex];
+	Sample current = sGen.GetNextSample();
+	Sample prev = current;
 	bool foundInflection = false;
 	bool foundPeak = false;
-	while (samplesRead > 0) {
-		for (size_t sampleOffset = 0; sampleOffset < samplesRead; sampleOffset += numChannels) {
-			auto sample = MapValueToSample(&dataBuffer[sampleOffset], channelIndex, sampleTimestamp);
+	while (current.timestamp != InvalidTS) {
 			if (foundInflection == false) {
-				foundInflection = findInflect(prev, sample);
+				foundInflection = findInflect(prev, current);
 			}
 			else if (foundPeak == false) {
-				foundPeak = peakPred(prev, sample);
+				foundPeak = peakPred(prev, current);
 			}
 
 			if (foundInflection && foundPeak) {
@@ -158,18 +276,15 @@ std::vector<Transition> GetTransitionsFromDAT2(const fs::path& filePath, int cha
 				transition.file = "DAT";
 				transition.on = on;
 				transition.timestamp = prev.timestamp;
-				transition.frameNumber = sampleIndex;
+				transition.frameNumber = 0;
 				transitions.emplace_back(transition);
 				foundInflection = false;
 				foundPeak = false;
 			}
 
-			prev = sample;
-			++sampleIndex;
-			sampleTimestamp = static_cast<uint64_t>(timePerSample * sampleIndex);
-		}
-		samplesRead = fread_s(dataBuffer.data(), dataBuffer.size() * sizeof(SampleValueType), sizeof(SampleValueType), samplesPerChunk, layFile);
+			prev = std::exchange(current, sGen.GetNextSample());
 	}
+
 	fclose(layFile);
 	return transitions;
 }
@@ -185,12 +300,17 @@ void main()
 	auto datTransitions2 = GetTransitionsFromDAT2(DATPath, 0, channelsInLay, timePerSample);
 
 
+	auto mismatched = std::mismatch(begin(datTransitions), end(datTransitions), begin(datTransitions2), end(datTransitions2), [](const auto& lhs, const auto& rhs) {
+		return lhs.timestamp == rhs.timestamp
+			&& lhs.on == rhs.on;
+	});
+
 	auto areEqual = std::equal(begin(datTransitions), end(datTransitions), begin(datTransitions2), end(datTransitions2), [](const auto& lhs, const auto& rhs) {
 		return lhs.timestamp == rhs.timestamp
 			&& lhs.on == rhs.on;
 	});
 
-	std::cout << (areEqual ? "Values equal" : "NOPE") << endl;
+	std::cout << (mismatched.first == end(datTransitions) ? "Values equal" : "NOPE") << endl;
 #ifdef _DEBUG
 	system("pause");
 #endif
